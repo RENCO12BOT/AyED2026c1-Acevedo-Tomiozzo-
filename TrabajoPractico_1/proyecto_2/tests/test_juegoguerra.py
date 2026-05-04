49e5302e61d89d59e66ef74a8a23e32106d9b99e
Franco Acevedo <franco10acevedo@gmail.com>
2026-05-04 00:11:52 -0300

    nueva actualizacion

--- TrabajoPractico_1/proyecto_2/tests/test_juegoguerra.py
+++ TrabajoPractico_1/proyecto_2/tests/test_juegoguerra.py
@@ -6,7 +6,7 @@ Created on Sun Aug 21 11:23:53 2022
 """
 
 
-from ..modules.guerra_de_cartas import JuegoGuerra
+from TrabajoPractico_1.proyecto_2.modules.guerra_de_cartas import JuegoGuerra
 import unittest
 
 

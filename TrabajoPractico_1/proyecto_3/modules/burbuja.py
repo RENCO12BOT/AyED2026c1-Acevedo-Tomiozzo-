27d46fdd1671cdf468d76cf745a1122cec8a033e
Franco Acevedo <franco10acevedo@gmail.com>
2026-05-13 12:56:31 -0300

    cambios

--- TrabajoPractico_1/proyecto_3/modules/burbuja.py
+++ TrabajoPractico_1/proyecto_3/modules/burbuja.py
@@ -6,12 +6,16 @@ class Burbuja:
         """Ordena la lista de manera ascendente usando burbuja."""
         contador = 0
         while contador < len(self.__lista):
+            esta_ordenada = True
             tamaño_lista = len(self.__lista) - contador
             for i in range(1, tamaño_lista):
                 if self.__lista[i] < self.__lista[i - 1]:
                     aux = self.__lista[i]
                     self.__lista[i] = self.__lista[i - 1]
                     self.__lista[i - 1] = aux
+                    esta_ordenada = False
+            if esta_ordenada:
+                break
             contador += 1
         return self.__lista
 
